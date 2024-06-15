8b885f47c5c8dd2455f5093b74b4403fe1924f01
eduardo.saatkamp <eduardosaatkamp@gmail.com>
2024-06-15 18:00:20 -0300

    atualizado calculo de area do triangulo

---
+++ areaTriangulo/emJavascript/areaTriangulo00.js
@@ -0,0 +1,7 @@
+var altura = 10;
+var base = 5;
+
+const area = (base * altura) / 2;
+
+console.log(`A área do triângulo é ${area}`);
+ 
---
+++ areaTriangulo/emJavascript/areaTriangulo01.js
@@ -0,0 +1,8 @@
+function CalcularAreaTriangulo(altura, base) {
+    return (altura*base)/2;
+}
+
+console.log(`A área do triângulo é: ${CalcularAreaTriangulo(10, 5)} unidades de área`);
+
+
+
